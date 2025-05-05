fc1674ea41a2ca515f01731b4508d5448a26da90
Abishek129 <abishek.reddy.020502@gmail.com>
2025-05-05 13:00:16 +0530

    cors1

--- future_pays/settings.py
+++ future_pays/settings.py
@@ -57,17 +57,7 @@ INSTALLED_APPS = [
     
 ]
 
-ASGI_APPLICATION = 'future_pays.asgi.application'
 
-
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
-        },
-    },
-}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
