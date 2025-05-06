1218d6f67df56ee6d8ea76f803f91f55efe08fb6
Abishek129 <abishek.reddy.020502@gmail.com>
2025-05-06 16:05:07 +0530

    redis_setup

--- future_pays/asgi.py
+++ future_pays/asgi.py
@@ -6,11 +6,20 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import your_app.routing  # Replace with your actual app name
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'future_pays.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),  # Handles normal Django views
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            your_app.routing.websocket_urlpatterns
+        )
+    ),
+})
+
